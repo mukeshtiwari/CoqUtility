29005e6493a6c1b3f586b1bbc16f32880c65c046
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-08-13 11:54:29 +1000

    Update Dependent.v

--- Dependent.v
+++ Dependent.v
@@ -23,3 +23,22 @@ Fixpoint lt {k} (a b: bits k) : Prop :=
     end amore
   end b.
   
+
+Definition pairop' {k} {rt} (a b: bits k)
+                     (r0: rt)
+                     (r1: forall k', bits k' -> bool -> bits k' -> bool -> (k = S k') -> rt) : rt :=
+   (* you must have the "as k''", and then the return must match it and not k *)
+   match k as k'' return (
+        match k'' with
+        | 0 => option ()
+        | S k' => option (bits k' * bool * bits k' * bool)
+        end) -> (k = k'') -> rt
+   with
+   | 0 => fun _ _ => r0
+   | S k' =>
+        fun H pf =>
+        match H with
+        | None => r0
+        | Some (amore, ax, bmore, bx) => r1 k' amore ax bmore bx pf
+        end
+   end (pairextract a b) eq_refl.
